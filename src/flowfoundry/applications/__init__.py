d76702030fb5a2f0bb8a9bd09054319e889cdb05
Ryan Shi <ryanshi1103@gmail.com>
2026-08-06 00:51:40 +0800

    feat(mediaflow): add sanitized application contract

---
+++ src/flowfoundry/applications/__init__.py
@@ -0,0 +1,5 @@
+"""Dependency-light contracts for applications integrated with FlowFoundry."""
+
+from .mediaflow import mediaflow_contract, validate_controlled_relative_path
+
+__all__ = ["mediaflow_contract", "validate_controlled_relative_path"]
